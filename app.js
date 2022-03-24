a2014d3a4afdf58cc85a1454d212bdee0d52d77c
Oleg <mykhailovych.oleh@comp-sc.if.ua>
2022-03-24 19:56:07 +0200

    updated app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const button = document.querySelector('#button');
 const usersContainer = document.querySelector('.users-container');
 const url = './static/users.json';
-
+let img;
 
 button.onclick = () => {
     getUsersInfo(url);
@@ -18,17 +18,16 @@ async function displayUsersInfo(data) {
     data.forEach(user => {
         usersContainer.innerHTML += `<br/><h3>User id:  ${user.id} </h3>  Name: ${user.first_name}<br/>
         Lastname: ${user.last_name}<br/> Email: ${user.email}<br/> ${getUserPhoto(user.photo)}`;
-        
-
     });
 }
 
-async function getUserPhoto(photo){
+async function getUserPhoto(photo) {
     let photoResponse = await fetch(photo)
     let blob = await photoResponse.blob();
 
-    
-    let img = document.createElement('img');
-    usersContainer.appendChild(img);
-    img.src=URL.createObjectURL(blob);
+
+    img = document.createElement('img');
+    img.src = URL.createObjectURL(blob);
+    document.body.append(img);
+
 }
