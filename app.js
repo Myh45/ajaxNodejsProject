ce4f04cf69e0cb474e6a9a9f65ee4fb0651a993c
Oleg <mykhailovych.oleh@comp-sc.if.ua>
2022-03-24 21:11:43 +0200

    deleted getUserPhoto()

--- app.js
+++ app.js
@@ -7,29 +7,25 @@ button.onclick = () => {
 }
 
 async function getUsersInfo(url) {
-    let response = await fetch(url);
-    let users = await response.json();
+    const response = await fetch(url);
+    const users = await response.json();
     displayUsersInfo(users);
 }
 
 async function displayUsersInfo(data) {
 
     data.forEach(user => {
-        usersContainer.innerHTML += `<br/><h3>User id:  ${user.id} </h3>  Name: ${user.first_name}<br/>
-        Lastname: ${user.last_name}<br/> Email: ${user.email}<br/>`;
-        getUserPhoto(user.photo);
-        
+        usersContainer.innerHTML += `
+        <br/>
+        <h3>
+            User id:  ${user.id}
+        </h3>
+        Name: ${user.first_name}
+        <br/>
+        Lastname: ${user.last_name}
+        <br/> 
+        Email: ${user.email}
+        <br/> 
+        <img src ="${user.photo}" alt="photo + ${user.id} ">`;
     });
-}
-
-async function getUserPhoto(photo) {
-    let photoResponse = await fetch(photo)
-    let blob = await photoResponse.blob();
-
-
-    img = document.createElement('img');
-    document.body.append(img);
-    
-    img.src = URL.createObjectURL(blob);
-
 }
